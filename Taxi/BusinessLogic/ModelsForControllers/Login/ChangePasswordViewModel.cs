a9a9b2cfc7dc8f8ef44661c25b2c913192632f8e
DmitriiPodlev <42978023+DmitriiPodlev@users.noreply.github.com>
2020-05-20 12:35:42 +0300

    Update views

--- Taxi/BusinessLogic/ModelsForControllers/Login/ChangePasswordViewModel.cs
+++ Taxi/BusinessLogic/ModelsForControllers/Login/ChangePasswordViewModel.cs
@@ -1,9 +1,17 @@
-﻿namespace BusinessLogic.ModelsForControllers
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace BusinessLogic.ModelsForControllers
 {
     public class ChangePasswordViewModel
     {
         public string Id { get; set; }
         public string Login { get; set; }
+        [Remote(action: "PasswordIsStrong", controller: "Account")]
+        [Required(ErrorMessage = "Не указан пароль")]
+        [MinLength(5, ErrorMessage = "Слишком короткий пароль")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Пароль")]
         public string NewPassword { get; set; }
         public string OldPassword { get; set; }
     }
